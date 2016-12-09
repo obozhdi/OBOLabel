4f9355e1014af447ca31c98bb355e5010031784c
obozhdi <obozhdi@obozhdi.com>
2016-12-09 23:06:30 +0200

    add simple effects

--- OBOLabel/OBOLabel/OBOLabel.swift
+++ OBOLabel/OBOLabel/OBOLabel.swift
@@ -7,8 +7,66 @@
 //
 
 import UIKit
+import QuartzCore
 
+@IBDesignable
 class OBOLabel: UILabel {
+    var isOutlined: Bool = false
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable override var shadowColor: UIColor? {
+        didSet {
+            layer.shadowColor = shadowColor?.cgColor
+        }
+    }
+    
+    @IBInspectable override var shadowOffset: CGSize {
+        didSet {
+            layer.shadowOffset = shadowOffset
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0.0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0.0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var masksToBounds: Bool = false {
+        didSet {
+            layer.masksToBounds = masksToBounds
+        }
+    }
+    
+    @IBInspectable var shouldRasterize: Bool = false {
+        didSet {
+            layer.shouldRasterize = shouldRasterize
+        }
+    }
 
     /*
     // Only override draw() if you perform custom drawing.
