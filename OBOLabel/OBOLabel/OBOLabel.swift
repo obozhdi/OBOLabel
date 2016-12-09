e172e286c8b7c84c7f9fe1ec8925ab7dd380a235
obozhdi <obozhdi@obozhdi.com>
2016-12-09 22:01:40 +0200

    add label class

---
+++ OBOLabel/OBOLabel/OBOLabel.swift
@@ -0,0 +1,21 @@
+//
+//  OBOLabel.swift
+//  OBOLabel
+//
+//  Created by Artem Pashkov on 09/12/2016.
+//  Copyright © 2016 obodev. All rights reserved.
+//
+
+import UIKit
+
+class OBOLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
