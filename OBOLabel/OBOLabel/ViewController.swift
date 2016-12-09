5bab7ef2970e027c94709c2e3c87ab74f238d2c2
obozhdi <obozhdi@obozhdi.com>
2016-12-09 21:44:19 +0200

    initial

---
+++ OBOLabel/OBOLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OBOLabel
+//
+//  Created by Artem Pashkov on 09/12/2016.
+//  Copyright © 2016 obodev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
